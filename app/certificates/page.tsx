3eefdb6635fd18f7961e540b97dfee58e60b3a81
Ashish agarwal <aviinashagarwal@gmail.com>
2026-01-31 13:40:05 +0530

    certificate

--- app/certificates/page.tsx
+++ app/certificates/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Image from 'next/image'
 
 const Certificates = () => {
@@ -10,6 +10,7 @@ const Certificates = () => {
     <div className=' h-screen w-full flex flex-col items-center justify-center '>Certificates
 
       <Image className='border-2 border-black p-2  rounded-md  bg-zinc-500/10 ' src="/isroCertificate.png" alt="certificate" width={500} height={500}/>
+      <Image className='border-2 border-black p-2  rounded-md  bg-zinc-500/10 ' src="/forage.jpeg" alt="certificate" width={500} height={500}/>
     </div>
   )
 }
